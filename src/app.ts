07f5852fb6321a796a1d16967bb3fa693bb75db5
bithighlander <bithighlander@gmail.com>
2023-05-15 19:12:50 -0600

    working

--- src/app.ts
+++ src/app.ts
@@ -12,8 +12,12 @@ import * as commander from 'commander';
 const program = new commander.Command();
 program.version('0.0.1').description(' Query the imported database for answers to your questions');
 
-export const run = async () => {
 
+/*
+    Build database
+ */
+
+export const run = async () => {
     //Instantiante the OpenAI model
     //Pass the "temperature" parameter which controls the RANDOMNESS of the model's output. A lower temperature will result in more predictable output, while a higher temperature will result in more random output. The temperature parameter is set between 0 and 1, with 0 being the most predictable and 1 being the most random
     const model = new OpenAI({ temperature: 0.9 });
@@ -38,7 +42,6 @@ export const run = async () => {
     const chain = VectorDBQAChain.fromLLM(model, vectorStore);
 
     program.parse(process.argv);
-    console.log(process.argv)
     let prompt = process.argv[2]
     console.log("prompt: ",prompt)
     const res = await chain.call({
